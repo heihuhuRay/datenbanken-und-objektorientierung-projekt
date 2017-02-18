c932df984a249ac829eae8609ee32e6936434deb
heihuhuRay <178200901@qq.com>
2017-02-18 01:09:06 +0100

    done with 4 relations

--- dboo_RMT.py
+++ dboo_RMT.py
@@ -9,7 +9,7 @@ from PyQt4.QtCore import *
 import sys
  
 
-#############################################################################################################################
+############################################################################################################################
 class E_Relation:
 	Property_list = []
 
@@ -27,23 +27,24 @@ class E_Relation:
 	def modify_P_Relation(self, old_P_name, new_P_name):
 		if '' == new_P_name:
 			print("Error: new_P_name can not be empty!")
-
+		is_correct_old_name = False
 		for p in self.Property_list:
 			if old_P_name == p:
 				p = new_P_name
-			else:
-				print("Error: old_P_name not corrected!")
- 
+				is_correct_old_name = True
+		if False == is_correct_old_name:
+			print("Error: old_P_name not corrected!")
 
 	def delete_P_Relation(self, P_name):
 		if '' == P_name:
 			print("Error: P_name can not be empty!")
-
+		is_correct_P_name = False
 		for p in self.Property_list:
 			if P_name == p:
 				self.Property_list.remove(P_name)
-			else:
-				print("Error: P_name not corrected!")
+				is_correct_P_name = True
+		if False == is_correct_P_name:
+			print("Error: P_name not corrected!")
 
 ############################################################################################################################
 def set_C_Relation(E1, E2):	#C_Relation: Characteristic Relation
@@ -53,27 +54,46 @@ def set_C_Relation(E1, E2):	#C_Relation: Characteristic Relation
 	C_Relation.append(C_dic)
 
 def set_A_Relation(E_list): #A_Relation: Associative Relation
-	A_Relation.append(E_list)
-
+	E_name_list = []
+	for e in E_list:
+		E_name_list.append(e.Entity_name)
+	A_Relation.append(E_name_list)
+###########################################################################################################################
 def show_E_Relation(K_list): 
 	app 	= QApplication(sys.argv)
 	table 	= QTableWidget()
 	tableItem 	= QTableWidgetItem()
-
 	# initiate table
 	table.setWindowTitle("E Relation")
 	table.resize(400, 250)
 	table.setRowCount(2*len(K_list))
 	table.setColumnCount(1)
-
 	# set data
 	for k in K_list:
 		table.setItem(2*K_list.index(k), 0, QTableWidgetItem(k+u'\u00a2')) # u'\u00a2' : ¢
-
 	#hide labels
 	table.verticalHeader().setVisible(False)
 	table.horizontalHeader().setVisible(False)
+	# show table
+	table.show()
+	return app.exec_()
 
+def show_P_Relation(E):
+	app 	= QApplication(sys.argv)
+	table 	= QTableWidget()
+	tableItem 	= QTableWidgetItem()
+	# initiate table
+	table.setWindowTitle("P Relation")
+	table.resize(400, 250)
+	table.setRowCount(2)
+	table.setColumnCount(len(E.Property_list)+1)
+	# set data
+	table.setItem(0, 0, QTableWidgetItem(E.Entity_name+u'\u00a2'))
+	for p in E.Property_list:
+		table.setItem(0, E.Property_list.index(p)+1, QTableWidgetItem(p)) # u'\u00a2' : ¢
+	#hide labels
+	table.verticalHeader().setVisible(False)
+	table.horizontalHeader().setVisible(False)
 	# show table
 	table.show()
 	return app.exec_()
@@ -118,7 +138,7 @@ def show_A_Relation(A_list):
 	# set data
 	for a in A_list:
 		for n in a:
-			table.setItem(A_list.index(a)*2, a.index(n), QTableWidgetItem(n))
+			table.setItem(0, a.index(n), QTableWidgetItem(n))
 	# table.setItem(0,0, QTableWidgetItem("Item (1,1)"))
 	# table.setItem(0,1, QTableWidgetItem("Item (1,2)"))
 	# table.setItem(1,0, QTableWidgetItem("Item (2,1)"))
@@ -159,6 +179,7 @@ if __name__ == '__main__':
 
 	E1 = E_Relation('Mobile') 
 	E1.add_P_Relation(['Name', 'ID'])
+	print(E1.Property_list)
 	E1.modify_P_Relation('ID', 'id_num')
 	Kernel_Entity.append(E1.Entity_name)
 
@@ -172,7 +193,9 @@ if __name__ == '__main__':
 	set_A_Relation([E1, E2, E3])
 
 	# show_E_Relation(Kernel_Entity)
+	show_P_Relation(E1)
 	# show_C_Relation(C_Relation)
+	print(A_Relation)
 	show_A_Relation(A_Relation)
 
 
