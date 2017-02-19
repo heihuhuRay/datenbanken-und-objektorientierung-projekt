d168320e0ac04406613beef8ca9d3be717828f07
heihuhuRay <178200901@qq.com>
2017-02-19 01:24:11 +0100

    finish lib and test demo

---
+++ Test_Demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dboo_RMT as RMT
+
+if __name__ == '__main__':
+	R1 = RMT.Relation()
+	E1 = RMT.E_Relation('Mobile') 
+	E1.add_P_Relation(['Name', 'ID'])
+	print(E1.Property_list)
+	E1.modify_P_Relation('ID', 'id_num')
+	R1.Kernel_Entity.append(E1.Entity_name)
+
+	E2 = RMT.E_Relation('Employee')
+	E2.add_P_Relation(['Job', 'Salary'])
+	R1.Kernel_Entity.append(E2.Entity_name)
+
+	E3 = RMT.E_Relation('Order')
+	R1.Kernel_Entity.append(E3.Entity_name)
+	R1.set_C_Relation(E1, E2)
+	R1.set_A_Relation([E1, E2, E3])
+
+	# show_E_Relation(R1.Kernel_Entity)
+	R1.show_P_Relation(E1)
+	# show_C_Relation(R1.C_Relation)
+	print(R1.A_Relation)
+	R1.show_A_Relation(R1.A_Relation)
--- dboo_RMT.py
+++ dboo_RMT.py
@@ -10,7 +10,7 @@ import sys
  
 
 ############################################################################################################################
-class E_Relation:
+class E_Relation(object):
 	Property_list = []
 
 	def __init__(self, Entity_name):
@@ -47,120 +47,125 @@ class E_Relation:
 			print("Error: P_name not corrected!")
 
 ############################################################################################################################
-def set_C_Relation(E1, E2):	#C_Relation: Characteristic Relation
-	C_dic = {'SUB': None, 'SUP': None}
-	C_dic['SUB'] = E1.Entity_name
-	C_dic['SUP'] = E2.Entity_name
-	C_Relation.append(C_dic)
+class Relation(object):
+	"""docstring for Relation"""
+	def __init__(self, Kernel_Entity = [], C_Relation = [], A_Relation = []):
+		self.Kernel_Entity = Kernel_Entity
+		self.C_Relation = C_Relation
+		self.A_Relation = A_Relation
+		
+	def set_C_Relation(self, E1, E2):	#C_Relation: Characteristic Relation
+		C_dic = {'SUB': None, 'SUP': None}
+		C_dic['SUB'] = E1.Entity_name
+		C_dic['SUP'] = E2.Entity_name
+		self.C_Relation.append(C_dic)
 
-def set_A_Relation(E_list): #A_Relation: Associative Relation
-	E_name_list = []
-	for e in E_list:
-		E_name_list.append(e.Entity_name)
-	A_Relation.append(E_name_list)
-###########################################################################################################################
-def show_E_Relation(K_list): 
-	app 	= QApplication(sys.argv)
-	table 	= QTableWidget()
-	tableItem 	= QTableWidgetItem()
-	# initiate table
-	table.setWindowTitle("E Relation")
-	table.resize(400, 250)
-	table.setRowCount(2*len(K_list))
-	table.setColumnCount(1)
-	# set data
-	for k in K_list:
-		table.setItem(2*K_list.index(k), 0, QTableWidgetItem(k+u'\u00a2')) # u'\u00a2' : ¢
-	#hide labels
-	table.verticalHeader().setVisible(False)
-	table.horizontalHeader().setVisible(False)
-	# show table
-	table.show()
-	return app.exec_()
+	def set_A_Relation(self, E_list): #A_Relation: Associative Relation
+		E_name_list = []
+		for e in E_list:
+			E_name_list.append(e.Entity_name)
+		self.A_Relation.append(E_name_list)
+	###########################################################################################################################
+	def show_E_Relation(self, K_list): 
+		app 	= QApplication(sys.argv)
+		table 	= QTableWidget()
+		tableItem 	= QTableWidgetItem()
+		# initiate table
+		table.setWindowTitle("E Relation")
+		table.resize(400, 250)
+		table.setRowCount(2*len(K_list))
+		table.setColumnCount(1)
+		# set data
+		for k in K_list:
+			table.setItem(2*K_list.index(k), 0, QTableWidgetItem(k+u'\u00a2')) # u'\u00a2' : ¢
+		#hide labels
+		table.verticalHeader().setVisible(False)
+		table.horizontalHeader().setVisible(False)
+		# show table
+		table.show()
+		return app.exec_()
 
-def show_P_Relation(E):
-	app 	= QApplication(sys.argv)
-	table 	= QTableWidget()
-	tableItem 	= QTableWidgetItem()
-	# initiate table
-	table.setWindowTitle("P Relation")
-	table.resize(400, 250)
-	table.setRowCount(2)
-	table.setColumnCount(len(E.Property_list)+1)
-	# set data
-	table.setItem(0, 0, QTableWidgetItem(E.Entity_name+u'\u00a2'))
-	for p in E.Property_list:
-		table.setItem(0, E.Property_list.index(p)+1, QTableWidgetItem(p)) # u'\u00a2' : ¢
-	#hide labels
-	table.verticalHeader().setVisible(False)
-	table.horizontalHeader().setVisible(False)
-	# show table
-	table.show()
-	return app.exec_()
+	def show_P_Relation(self, E):
+		app 	= QApplication(sys.argv)
+		table 	= QTableWidget()
+		tableItem 	= QTableWidgetItem()
+		# initiate table
+		table.setWindowTitle("P Relation")
+		table.resize(400, 250)
+		table.setRowCount(2)
+		table.setColumnCount(len(E.Property_list)+1)
+		# set data
+		table.setItem(0, 0, QTableWidgetItem(E.Entity_name+u'\u00a2'))
+		for p in E.Property_list:
+			table.setItem(0, E.Property_list.index(p)+1, QTableWidgetItem(p)) # u'\u00a2' : ¢
+		#hide labels
+		table.verticalHeader().setVisible(False)
+		table.horizontalHeader().setVisible(False)
+		# show table
+		table.show()
+		return app.exec_()
 
-def show_C_Relation(C_list):
-	app 	= QApplication(sys.argv)
-	table 	= QTableWidget()
-	tableItem 	= QTableWidgetItem()
+	def show_C_Relation(self, C_list):
+		app 	= QApplication(sys.argv)
+		table 	= QTableWidget()
+		tableItem 	= QTableWidgetItem()
 
-	# initiate table
-	table.setWindowTitle("Characteristic Graph Relation")
-	table.resize(400, 250)
-	table.setRowCount(len(C_list)+1)
-	table.setColumnCount(2)
+		# initiate table
+		table.setWindowTitle("Characteristic Graph Relation")
+		table.resize(400, 250)
+		table.setRowCount(len(C_list)+1)
+		table.setColumnCount(2)
 
-	# set data
-	table.setItem(0,0, QTableWidgetItem('SUB'))
-	table.setItem(0,1, QTableWidgetItem('SUP'))
-	for c in C_list:
-		table.setItem(C_list.index(c)+1, 0, QTableWidgetItem(c['SUB']))
-		table.setItem(C_list.index(c)+1, 1, QTableWidgetItem(c['SUP']))
+		# set data
+		table.setItem(0,0, QTableWidgetItem('SUB'))
+		table.setItem(0,1, QTableWidgetItem('SUP'))
+		for c in C_list:
+			table.setItem(C_list.index(c)+1, 0, QTableWidgetItem(c['SUB']))
+			table.setItem(C_list.index(c)+1, 1, QTableWidgetItem(c['SUP']))
 
-	#hide labels
-	table.verticalHeader().setVisible(False)
-	table.horizontalHeader().setVisible(False)
+		#hide labels
+		table.verticalHeader().setVisible(False)
+		table.horizontalHeader().setVisible(False)
 
-	# show table
-	table.show()
-	return app.exec_()
+		# show table
+		table.show()
+		return app.exec_()
 
-def show_A_Relation(A_list):
-	app 	= QApplication(sys.argv)
-	table 	= QTableWidget()
-	tableItem 	= QTableWidgetItem()
+	def show_A_Relation(self, A_list):
+		app 	= QApplication(sys.argv)
+		table 	= QTableWidget()
+		tableItem 	= QTableWidgetItem()
 
-	# initiate table
-	table.setWindowTitle("Associative Graph Relation")
-	table.resize(400, 250)
-	table.setRowCount(2)
-	table.setColumnCount(3)
+		# initiate table
+		table.setWindowTitle("Associative Graph Relation")
+		table.resize(400, 250)
+		table.setRowCount(2)
+		table.setColumnCount(3)
 
-	# set data
-	for a in A_list:
-		for n in a:
-			table.setItem(0, a.index(n), QTableWidgetItem(n))
-	# table.setItem(0,0, QTableWidgetItem("Item (1,1)"))
-	# table.setItem(0,1, QTableWidgetItem("Item (1,2)"))
-	# table.setItem(1,0, QTableWidgetItem("Item (2,1)"))
-	# table.setItem(1,1, QTableWidgetItem("Item (2,2)"))
-	# table.setItem(2,0, QTableWidgetItem("Item (3,1)"))
-	# table.setItem(2,1, QTableWidgetItem("Item (3,2)"))
-	# table.setItem(3,0, QTableWidgetItem("Item (4,1)"))
-	# table.setItem(3,1, QTableWidgetItem("Item (4,2)"))
-	#hide labels
-	table.verticalHeader().setVisible(False)
-	table.horizontalHeader().setVisible(False)
-	# show table
-	table.show()
-	return app.exec_()
+		# set data
+		for a in A_list:
+			for n in a:
+				table.setItem(0, a.index(n), QTableWidgetItem(n))
+		# table.setItem(0,0, QTableWidgetItem("Item (1,1)"))
+		# table.setItem(0,1, QTableWidgetItem("Item (1,2)"))
+		# table.setItem(1,0, QTableWidgetItem("Item (2,1)"))
+		# table.setItem(1,1, QTableWidgetItem("Item (2,2)"))
+		# table.setItem(2,0, QTableWidgetItem("Item (3,1)"))
+		# table.setItem(2,1, QTableWidgetItem("Item (3,2)"))
+		# table.setItem(3,0, QTableWidgetItem("Item (4,1)"))
+		# table.setItem(3,1, QTableWidgetItem("Item (4,2)"))
+		#hide labels
+		table.verticalHeader().setVisible(False)
+		table.horizontalHeader().setVisible(False)
+		# show table
+		table.show()
+		return app.exec_()
 
 
 	
 
 
-if __name__ == '__main__':
-	Kernel_Entity = [] 
-	C_Relation = []
+# if __name__ == '__main__':
 	# [ {
 	#	 'SUB': 'Mobile',
 	#	 'SUP': 'Name'
@@ -170,33 +175,35 @@ if __name__ == '__main__':
 	#	 'SUP': 'ID'
 	#	} 
 	#	]
-	A_Relation = []	
+	
 	# [
 	# 	['Mobile', 'Name', 'ID'],
 	# 	['Employee', 'Job', 'Salary']
 	# ]
+	# Kernel_Entity = [] 
+	# C_Relation = []
+	# A_Relation = []	
 
+	# E1 = E_Relation('Mobile') 
+	# E1.add_P_Relation(['Name', 'ID'])
+	# print(E1.Property_list)
+	# E1.modify_P_Relation('ID', 'id_num')
+	# Kernel_Entity.append(E1.Entity_name)
 
-	E1 = E_Relation('Mobile') 
-	E1.add_P_Relation(['Name', 'ID'])
-	print(E1.Property_list)
-	E1.modify_P_Relation('ID', 'id_num')
-	Kernel_Entity.append(E1.Entity_name)
+	# E2 = E_Relation('Employee')
+	# E2.add_P_Relation(['Job', 'Salary'])
+	# Kernel_Entity.append(E2.Entity_name)
 
-	E2 = E_Relation('Employee')
-	E2.add_P_Relation(['Job', 'Salary'])
-	Kernel_Entity.append(E2.Entity_name)
+	# E3 = E_Relation('Order')
+	# Kernel_Entity.append(E3.Entity_name)
+	# set_C_Relation(E1, E2)
+	# set_A_Relation([E1, E2, E3])
 
-	E3 = E_Relation('Order')
-	Kernel_Entity.append(E3.Entity_name)
-	set_C_Relation(E1, E2)
-	set_A_Relation([E1, E2, E3])
-
-	# show_E_Relation(Kernel_Entity)
-	show_P_Relation(E1)
-	# show_C_Relation(C_Relation)
-	print(A_Relation)
-	show_A_Relation(A_Relation)
+	# # show_E_Relation(Kernel_Entity)
+	# show_P_Relation(E1)
+	# # show_C_Relation(C_Relation)
+	# print(A_Relation)
+	# show_A_Relation(A_Relation)
 
 
 
